c0ab25e8b2acad1fc3b0844f4d0c659966c4f2d7
anjana6 <anjanashakthi95@gmail.com>
2020-10-07 23:17:48 +0530

    add the working email and password for login

--- eCormmerce/src/index.ts
+++ eCormmerce/src/index.ts
@@ -3,6 +3,6 @@ import AddToCartController from './controller/Cart.controller';
 import AuthenticationController from './controller/authentication.controller';
 import ItemController from './controller/item.controller';
 
-const app = new App([new ItemController,new AuthenticationController,new AddToCartController],5000);
+const app = new App([new ItemController,new AuthenticationController,new AddToCartController],4000);
 
 app.listen();
